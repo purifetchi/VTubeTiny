643d5a5148eee1672ba1ee4d70ef4d4f895a6039
naomiEve <0xlunaric@gmail.com>
2022-11-27 20:05:50 +0100

    feat!: add drag and dropping images into the editor's scene.
    doing so will instantiate a new actor with a texture renderer set to
    the dropped image.

--- VTTiny/Editor/VTubeTinyEditor.cs
+++ VTTiny/Editor/VTubeTinyEditor.cs
@@ -1,5 +1,7 @@
 ﻿using ImGuiNET;
+using Raylib_cs;
 using rlImGui_cs;
+using VTTiny.Components;
 
 namespace VTTiny.Editor
 {
@@ -34,10 +36,31 @@ namespace VTTiny.Editor
             ImGui.Begin("VTubeTiny Editor");
 
             VTubeTiny.ActiveStage.RenderEditorGUI();
+            HandleDragAndDropImages();
 
             ImGui.End();
 
             rlImGui.End();
+        }
+
+        /// <summary>
+        /// Handles drag and dropping images into the VTubeTiny stage. Will automatically instantiate a
+        /// new actor with a texture renderer set to the dragged image.
+        /// 
+        /// NOTE: Drag and dropping images onto texture buttons takes precedence over this behavior.
+        /// </summary>
+        private void HandleDragAndDropImages()
+        {
+            if (!Raylib.IsFileDropped())
+                return;
+
+            var path = Raylib.GetDroppedFiles()[0];
+            Raylib.ClearDroppedFiles();
+
+            var renderer = VTubeTiny.ActiveStage.CreateActor()
+                                                .AddComponent<TextureRendererComponent>();
+
+            renderer.Texture = Raylib.LoadTexture(path);
         }
 
         /// <summary>
