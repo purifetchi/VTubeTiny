88431fe20685148c314ccb8ac675a2b5936508ca
naomiEve <0xlunaric@gmail.com>
2022-11-30 13:26:43 +0100

    feat: add a texture class.
    this is an abstraction on top of raylib's texture2d, that automatically
    manages the texture's lifetime.
    
    it also stores the path to the texture for serialization.

---
+++ VTTiny/Base/Texture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Raylib_cs;
+
+namespace VTTiny
+{
+    public class Texture : IDisposable
+    {
+        /// <summary>
+        /// The actual Raylib texture.
+        /// </summary>
+        public Texture2D BackingTexture { get; private set; }
+
+        /// <summary>
+        /// The path to the texture on disk.
+        /// </summary>
+        public string Path { get; private set; }
+
+        /// <summary>
+        /// Constructs a texture loading it from a path.
+        /// </summary>
+        /// <param name="path">The path to the texture.</param>
+        public Texture(string path)
+        {
+            LoadTexture(path);
+        }
+
+        /// <summary>
+        /// Loads a texture from a given path.
+        /// </summary>
+        /// <param name="path">The path to the texture.</param>
+        private void LoadTexture(string path)
+        {
+            BackingTexture = Raylib.LoadTexture(path);
+            Path = path;
+        }
+
+        public void Dispose()
+        {
+            Raylib.UnloadTexture(BackingTexture);
+        }
+    }
+}
