1035fcef01cd0deb9c4260c9edcf37949dbfdb7c
naomiEve <0xlunaric@gmail.com>
2022-12-04 19:55:37 +0100

    feat: implement the win32 api for opening file dialog boxes.
    this is only for win32 so far, but i'm yearning for a more crossplatform
    solution.
    
    used by the editor.

---
+++ VTTiny/Editor/Native/Win32/FileDialog.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VTTiny.Editor.Native.Win32
+{
+    /// <summary>
+    /// This class wraps the Win32 methods for opening open/save file dialog boxes.
+    /// 
+    /// NOTE: So far this is the only FileDialog implementation we have, it'd be nice to have more cross platform solutions.
+    ///       PRs welcome.
+    /// </summary>
+    public static class FileDialog
+    {
+        [DllImport("comdlg32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool GetOpenFileName(ref OpenFileName ofn);
+
+        /// <summary>
+        /// Show an open file dialog.
+        /// </summary>
+        /// <returns>The path to the opened file, or nothing.</returns>
+        public static string OpenFile()
+        {
+            var ofn = OpenFileName.Create();
+            if (GetOpenFileName(ref ofn))
+                return ofn.file;
+
+            return string.Empty;
+        }
+
+        [DllImport("Comdlg32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool GetSaveFileName(ref OpenFileName ofn);
+
+        /// <summary>
+        /// Show a save file dialog.
+        /// </summary>
+        /// <returns>The path to the saved file, or nothing.</returns>
+        public static string SaveFile()
+        {
+            var ofn = OpenFileName.Create();
+            if (GetSaveFileName(ref ofn))
+                return ofn.file;
+
+            return string.Empty;
+        }
+    }
+}
---
+++ VTTiny/Editor/Native/Win32/OpenFileName.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace VTTiny.Editor.Native.Win32
+{
+    /// <summary>
+    /// The OpenFileDialog struct required for calling GetOpenFileName.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    internal struct OpenFileName
+    {
+        /// <summary>
+        /// Constructs a new OpenFileName struct with generic data.
+        /// </summary>
+        /// <returns>The OpenFileName struct.</returns>
+        public static OpenFileName Create()
+        {
+            var ofn = new OpenFileName
+            {
+                structSize = Marshal.SizeOf<OpenFileName>(),
+                filter = "Scene data\0*.json",
+                defExt = "json"
+            };
+
+
+            ofn.file = new string(new char[256]);
+            ofn.maxFile = ofn.file.Length;
+
+            ofn.fileTitle = new string(new char[64]);
+            ofn.maxFileTitle = ofn.fileTitle.Length;
+
+            return ofn;
+        }
+
+        public int structSize;
+        public IntPtr dlgOwner;
+        public IntPtr instance;
+
+        public string filter;
+        public string customFilter;
+        public int maxCustFilter;
+        public int filterIndex;
+
+        public string file;
+        public int maxFile;
+
+        public string fileTitle;
+        public int maxFileTitle;
+
+        public string initialDir;
+
+        public string title;
+
+        public int flags;
+        public short fileOffset;
+        public short fileExtension;
+
+        public string defExt;
+
+        public IntPtr custData;
+        public IntPtr hook;
+
+        public string templateName;
+
+        public IntPtr reservedPtr;
+        public int reservedInt;
+        public int flagsEx;
+    }
+}
