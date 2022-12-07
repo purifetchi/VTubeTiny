7225c61acc9e95c4a13ee11a2de46a7dbdee2dd1
naomiEve <0xlunaric@gmail.com>
2022-12-07 12:32:38 +0100

    feat+refactor: add a menu bar/category abstraction.
    this will allow us to abstract how the main menu works like, instead
    of putting all of the code in VTubeTinyEditor.cs

---
+++ VTTiny/Editor/UI/MenuBar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using ImGuiNET;
+
+namespace VTTiny.Editor.UI
+{
+    /// <summary>
+    /// This is an abstraction over the ImGui main menu bar.
+    /// </summary>
+    internal class MenuBar
+    {
+        /// <summary>
+        /// The editor this main menu bar is attached to.
+        /// </summary>
+        public VTubeTinyEditor Editor { get; private set; }
+
+        /// <summary>
+        /// A list of all categories.
+        /// </summary>
+        private readonly List<MenuCategory> _categories;
+
+        /// <summary>
+        /// Constructs a new menu bar.
+        /// </summary>
+        public MenuBar(VTubeTinyEditor editor)
+        {
+            Editor = editor;
+            _categories = new();
+        }
+
+        /// <summary>
+        /// Adds a category to this menu bar.
+        /// </summary>
+        /// <param name="category">The category.</param>
+        public void AddCategory(MenuCategory category)
+        {
+            _categories.Add(category);
+        }
+
+        /// <summary>
+        /// Renders the menu.
+        /// </summary>
+        public void Render()
+        {
+            if (!ImGui.BeginMainMenuBar())
+                return;
+
+            foreach (var category in _categories)
+                category.Render();
+
+            ImGui.EndMainMenuBar();
+        }
+    }
+}
---
+++ VTTiny/Editor/UI/MenuCategory.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using ImGuiNET;
+
+namespace VTTiny.Editor.UI
+{
+    /// <summary>
+    /// A singular category for a menu.
+    /// </summary>
+    internal class MenuCategory
+    {
+        /// <summary>
+        /// A single action for the menu category.
+        /// </summary>
+        private class MenuAction
+        {
+            /// <summary>
+            /// The name of the action.
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            /// The callback for the action.
+            /// </summary>
+            public Action<VTubeTinyEditor> Callback { get; set; }
+        }
+
+        /// <summary>
+        /// The name of this category.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The editor.
+        /// </summary>
+        public VTubeTinyEditor Editor { get; set; }
+
+        /// <summary>
+        /// The list of actions within this menu.
+        /// </summary>
+        private readonly List<MenuAction> _actions;
+
+        /// <summary>
+        /// Creates a new menu category.
+        /// </summary>
+        /// <param name="name">The name of the category.</param>
+        public MenuCategory(string name, VTubeTinyEditor editor)
+        {
+            Name = name;
+            Editor = editor;
+            _actions = new();
+        }
+
+        /// <summary>
+        /// Adds a new action to this menu category.
+        /// </summary>
+        /// <param name="name">The name of this action.</param>
+        /// <param name="callback">The callback.</param>
+        public void AddAction(string name, Action<VTubeTinyEditor> callback)
+        {
+            _actions.Add(new MenuAction
+            {
+                Name = name,
+                Callback = callback
+            });
+        }
+
+        /// <summary>
+        /// Renders this menu category.
+        /// </summary>
+        public void Render()
+        {
+            if (!ImGui.BeginMenu(Name))
+                return;
+
+            foreach (var action in _actions)
+            {
+                if (ImGui.MenuItem(action.Name))
+                    action.Callback?.Invoke(Editor);
+            }
+
+            ImGui.EndMenu();
+        }
+    }
+}
