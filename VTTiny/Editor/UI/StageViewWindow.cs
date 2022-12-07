53f9f32e86edac62f82b124474ac48261e6508fc
naomiEve <0xlunaric@gmail.com>
2022-12-07 11:12:54 +0100

    feat+refactor!: move windows into their own classes.
    we no longer have the systems for all windows inside of VTubeTinyEditor.cs

---
+++ VTTiny/Editor/UI/StageViewWindow.cs
@@ -0,0 +1,192 @@
+﻿using ImGuiNET;
+using Raylib_cs;
+using rlImGui_cs;
+using VTTiny.Components;
+using VTTiny.Rendering;
+using VTTiny.Scenery;
+
+namespace VTTiny.Editor.UI
+{
+    /// <summary>
+    /// The stage view.
+    /// </summary>
+    internal class StageViewWindow : EditorWindow, IStageAwareWindow
+    {
+        /// <summary>
+        /// The stage this window is showing.
+        /// </summary>
+        public Stage Stage { get; private set; }
+
+        /// <summary>
+        /// The actor that was last held while moving.
+        /// </summary>
+        private StageActor _heldActor = null;
+
+        /// <summary>
+        /// The last mouse position while moving.
+        /// </summary>
+        private Vector2Int _lastMousePosition;
+
+        /// <summary>
+        /// Creates a new stage view for a given stage.
+        /// </summary>
+        /// <param name="stage">The stage to show.</param>
+        public StageViewWindow(Stage stage) 
+            : base("Stage View", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoResize)
+        {
+            SetStage(stage);
+        }
+
+        /// <summary>
+        /// Replaces the visible stage.
+        /// </summary>
+        /// <param name="stage">The new stage to show.</param>
+        public void SetStage(Stage stage)
+        {
+            if (stage.RenderingContext is not FramebufferRenderingContext)
+                stage.ReplaceRenderingContext(new FramebufferRenderingContext());
+
+            Stage = stage;
+        }
+
+        /// <summary>
+        /// Tries to get the position of a mouse within a stage.
+        /// </summary>
+        /// <param name="absolute">The absolute position of the mouse.</param>
+        /// <param name="relative">The relative position within the stage view.</param>
+        /// <returns>Whether the mouse even was in the stage.</returns>
+        private bool TryGetPositionWithinStage(Vector2Int absolute, out Vector2Int relative)
+        {
+            var pos = ImGui.GetItemRectMin();
+            var size = ImGui.GetItemRectSize();
+
+            var rect = new Rectangle
+            {
+                x = pos.X,
+                y = pos.Y,
+                height = size.Y,
+                width = size.X
+            };
+
+            relative = absolute - (Vector2Int)pos;
+            return Raylib.CheckCollisionPointRec(absolute, rect);
+        }
+
+        /// <summary>
+        /// Handles drag and dropping images into the VTubeTiny stage. Will automatically instantiate a
+        /// new actor with a texture renderer set to the dragged image.
+        /// 
+        /// NOTE: Drag and dropping images onto texture buttons takes precedence over this behavior.
+        /// </summary>
+        private void HandleDragAndDropImages()
+        {
+            if (!Raylib.IsFileDropped() || !TryGetPositionWithinStage(Raylib.GetMousePosition(), out Vector2Int mousePos))
+                return;
+
+            var path = Raylib.GetDroppedFiles()[0];
+            Raylib.ClearDroppedFiles();
+
+            var actor = Stage.CreateActor();
+            var renderer = actor.AddComponent<TextureRendererComponent>();
+
+            var texture = new Texture(path);
+            renderer.Texture = texture;
+
+            // Offset the mouse cursor by half of the texture's size, putting the actor's center
+            // at the position of the mouse.
+            mousePos -= new Vector2Int(texture.Width / 2, texture.Height / 2);
+
+            actor.Transform.LocalPosition = mousePos;
+        }
+
+        /// <summary>
+        /// Handles dragging actors around with the mouse.
+        /// </summary>
+        private void HandleActorDragging()
+        {
+            var absolutePos = (Vector2Int)Raylib.GetMousePosition();
+            if (!TryGetPositionWithinStage(absolutePos, out Vector2Int relativePos))
+                return;
+
+            if (_heldActor == null)
+            {
+                if (!TryFindNewActorForDragging(relativePos))
+                    return;
+            }
+
+            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
+            {
+                _heldActor.RenderBoundingBox();
+
+                var delta = relativePos - _lastMousePosition;
+                _heldActor.Transform.LocalPosition = _heldActor.Transform.LocalPosition + delta;
+
+                _lastMousePosition = relativePos;
+                return;
+            }
+
+            _heldActor = null;
+        }
+
+        /// <summary>
+        /// Tries to get a new actor for dragging.
+        /// </summary>
+        /// <param name="position">The position to hit test from.</param>
+        /// <returns>True if found, false otherwise.</returns>
+        private bool TryFindNewActorForDragging(Vector2Int position)
+        {
+            if (!Raylib.IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
+                return false;
+
+            var actor = Stage.HitTest(position);
+            if (actor == null)
+                return false;
+
+            _heldActor = actor;
+            _lastMousePosition = position;
+            return true;
+        }
+
+        protected override void PreDrawUI()
+        {
+            var frameBuffer = Stage.RenderingContext.GetFramebuffer().Value;
+            var size = new Vector2Int(frameBuffer.width, frameBuffer.height);
+
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2Int(0, 0));
+            ImGui.SetNextWindowSizeConstraints(size, size);
+        }
+
+        protected override void DrawUI()
+        {
+            var frameBuffer = Stage.RenderingContext.GetFramebuffer().Value;
+            var size = new Vector2Int(frameBuffer.width, frameBuffer.height);
+
+            // Transform the render texture, since it's drawn upside down.
+            var viewRect = new Rectangle
+            {
+                x = frameBuffer.width / 2 - size.X / 2,
+                y = frameBuffer.height / 2 - size.Y / 2,
+                width = size.X,
+                height = -size.Y
+            };
+
+            // Draw the buffer in the center of the screen
+            var center = (Vector2Int)((ImGui.GetWindowSize() - (System.Numerics.Vector2)size) * 0.5f);
+            ImGui.SetCursorPos(center);
+            rlImGui.ImageRect(frameBuffer, size.X, size.Y, viewRect);
+
+            HandleDragAndDropImages();
+            HandleActorDragging();
+        }
+
+        protected override void PostDrawUI()
+        {
+            ImGui.PopStyleVar();
+        }
+
+        public void OnStageChange(Stage stage)
+        {
+            SetStage(stage);
+        }
+    }
+}
--- VTTiny/Editor/VTubeTinyEditor.cs
+++ VTTiny/Editor/VTubeTinyEditor.cs
@@ -1,11 +1,13 @@
-﻿using ImGuiNET;
+﻿using System;
+using System.Collections.Generic;
+using ImGuiNET;
 using Raylib_cs;
 using rlImGui_cs;
-using VTTiny.Components;
 using VTTiny.Editor.Native;
+using VTTiny.Editor.Native.Win32;
+using VTTiny.Editor.UI;
 using VTTiny.Rendering;
 using VTTiny.Scenery;
-using VTTiny.Editor.Native.Win32;
 
 namespace VTTiny.Editor
 {
@@ -14,12 +16,17 @@ namespace VTTiny.Editor
     /// </summary>
     internal class VTubeTinyEditor
     {
+        /// <summary>
+        /// The current instance of VTubeTiny this editor is working with.
+        /// </summary>
         private VTubeTiny VTubeTiny { get; set; }
 
+        /// <summary>
+        /// The rendering context of the window.
+        /// </summary>
         private IRenderingContext RenderingContext { get; set; }
 
-        private StageActor _heldActor = null;
-        private Vector2Int _lastMousePosition;
+        private readonly List<EditorWindow> _windows;
         private bool _didLayoutEditorDocks = false;
 
         /// <summary>
@@ -30,7 +37,47 @@ namespace VTTiny.Editor
         {
             VTubeTiny = instance;
             RenderingContext = new GenericRaylibRenderingContext();
+            _windows = new List<EditorWindow>();
         }
+
+        /// <summary>
+        /// Adds a new window to the window list.
+        /// </summary>
+        /// <param name="window">The window.</param>
+        private void AddWindow(EditorWindow window)
+        {
+            _windows.Add(window);
+        }
+
+        /// <summary>
+        /// Gets a window by its type.
+        /// </summary>
+        /// <typeparam name="T">The type of the window (must derive from `VTTiny.Editor.UI.EditorWindow`.</typeparam>
+        /// <returns>Either the window or null.</returns>
+        private T GetWindow<T>() where T: EditorWindow
+        {
+            foreach (var window in _windows)
+            {
+                if (window is T typedWindow)
+                    return typedWindow;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Executes a callback for each window that derives from a type.
+        /// </summary>
+        /// <typeparam name="T">The type the window should derive from.</typeparam>
+        /// <param name="callback">The callback.</param>
+        private void ForEachWindowOfType<T>(Action<T> callback)
+        {
+            foreach (var window in _windows)
+            {
+                if (window is T type)
+                    callback(type);
+            }
+        } 
 
         /// <summary>
         /// Initializes the editor and all of the systems it needs.
@@ -44,8 +91,8 @@ namespace VTTiny.Editor
             Raylib.SetWindowState(ConfigFlags.FLAG_WINDOW_RESIZABLE);
             Raylib.MaximizeWindow();
 
-            // Switch the stage to running in framebuffer mode, so we can draw it as a window.
-            VTubeTiny.ActiveStage.ReplaceRenderingContext(new FramebufferRenderingContext());
+            AddWindow(new StageViewWindow(VTubeTiny.ActiveStage));
+            AddWindow(new StagePropertiesWindow(VTubeTiny.ActiveStage));
         }
 
         /// <summary>
@@ -61,8 +108,9 @@ namespace VTTiny.Editor
             var dockId = ImGui.DockSpaceOverViewport();
 
             DrawMainMenuBar();
-            DrawEditorView();
-            DrawStageView();
+
+            foreach (var window in _windows)
+                window.Render();
 
             if (!_didLayoutEditorDocks)
                 LayoutDockWindows(dockId);
@@ -84,9 +132,9 @@ namespace VTTiny.Editor
                     if (ImGui.MenuItem("New stage"))
                     {
                         var stage = Stage.Blank();
-                        stage.ReplaceRenderingContext(new FramebufferRenderingContext());
-
                         VTubeTiny.SetActiveStage(stage);
+
+                        ForEachWindowOfType<IStageAwareWindow>(window => window.OnStageChange(VTubeTiny.ActiveStage));
                     }
 
                     if (ImGui.MenuItem("Load stage"))
@@ -97,7 +145,8 @@ namespace VTTiny.Editor
 
                         VTubeTiny.LoadConfigFromFile(path);
                         VTubeTiny.ReloadStage();
-                        VTubeTiny.ActiveStage.ReplaceRenderingContext(new FramebufferRenderingContext());
+
+                        ForEachWindowOfType<IStageAwareWindow>(window => window.OnStageChange(VTubeTiny.ActiveStage));
                     }
 
                     if (ImGui.MenuItem("Save stage"))
@@ -131,163 +180,16 @@ namespace VTTiny.Editor
         }
 
         /// <summary>
-        /// Draws the stage view.
-        /// </summary>
-        private void DrawStageView()
-        {
-            var frameBuffer = VTubeTiny.ActiveStage.RenderingContext.GetFramebuffer().Value;
-            var size = new Vector2Int(frameBuffer.width, frameBuffer.height);
-
-            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2Int(0, 0));
-
-            ImGui.SetNextWindowSizeConstraints(size, size);
-            if (ImGui.Begin("Stage View", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar))
-            {
-                // Transform the render texture, since it's drawn upside down.
-                var viewRect = new Rectangle
-                {
-                    x = frameBuffer.width / 2 - size.X / 2,
-                    y = frameBuffer.height / 2 - size.Y / 2,
-                    width = size.X,
-                    height = -size.Y
-                };
-
-                // Draw the buffer in the center of the screen
-                var center = (Vector2Int)((ImGui.GetWindowSize() - (System.Numerics.Vector2)size) * 0.5f);
-                ImGui.SetCursorPos(center);
-                rlImGui.ImageRect(frameBuffer, size.X, size.Y, viewRect);
-
-                HandleDragAndDropImages();
-                HandleActorDragging();
-
-                ImGui.End();
-            }
-
-            ImGui.PopStyleVar();
-        }
-
-        /// <summary>
-        /// Draws the editor view.
-        /// </summary>
-        private void DrawEditorView()
-        {
-            if (ImGui.Begin("VTubeTiny Editor"))
-            {
-                VTubeTiny.ActiveStage.RenderEditorGUI();
-
-                ImGui.End();
-            }
-        }
-
-        /// <summary>
         /// Lays out the docked windows.
         /// </summary>
         private void LayoutDockWindows(uint dockId)
         {
             var editorDockId = ImGuiDockBuilder.SplitNode(dockId, ImGuiDir.Left, 0.8f, out uint _, out uint _);
-            ImGuiDockBuilder.DockWindow("VTubeTiny Editor", dockId);
-            ImGuiDockBuilder.DockWindow("Stage View", editorDockId);
+            GetWindow<StagePropertiesWindow>().Dock(dockId);
+            GetWindow<StageViewWindow>().Dock(editorDockId);
             ImGuiDockBuilder.Finish(dockId);
 
             _didLayoutEditorDocks = true;
-        }
-
-        /// <summary>
-        /// Tries to get the position of a mouse within a stage.
-        /// </summary>
-        /// <param name="absolute">The absolute position of the mouse.</param>
-        /// <param name="relative">The relative position within the stage view.</param>
-        /// <returns>Whether the mouse even was in the stage.</returns>
-        private bool TryGetPositionWithinStage(Vector2Int absolute, out Vector2Int relative)
-        {
-            var pos = ImGui.GetItemRectMin();
-            var size = ImGui.GetItemRectSize();
-
-            var rect = new Rectangle
-            {
-                x = pos.X,
-                y = pos.Y,
-                height = size.Y,
-                width = size.X
-            };
-
-            relative = absolute - (Vector2Int)pos;
-            return Raylib.CheckCollisionPointRec(absolute, rect);
-        }
-
-        /// <summary>
-        /// Handles drag and dropping images into the VTubeTiny stage. Will automatically instantiate a
-        /// new actor with a texture renderer set to the dragged image.
-        /// 
-        /// NOTE: Drag and dropping images onto texture buttons takes precedence over this behavior.
-        /// </summary>
-        private void HandleDragAndDropImages()
-        {
-            if (!Raylib.IsFileDropped() || !TryGetPositionWithinStage(Raylib.GetMousePosition(), out Vector2Int mousePos))
-                return;
-
-            var path = Raylib.GetDroppedFiles()[0];
-            Raylib.ClearDroppedFiles();
-
-            var actor = VTubeTiny.ActiveStage.CreateActor();
-            var renderer = actor.AddComponent<TextureRendererComponent>();
-
-            var texture = new Texture(path);
-            renderer.Texture = texture;
-
-            // Offset the mouse cursor by half of the texture's size, putting the actor's center
-            // at the position of the mouse.
-            mousePos -= new Vector2Int(texture.Width / 2, texture.Height / 2);
-
-            actor.Transform.LocalPosition = mousePos;
-        }
-
-        /// <summary>
-        /// Handles dragging actors around with the mouse.
-        /// </summary>
-        private void HandleActorDragging()
-        {
-            var absolutePos = (Vector2Int)Raylib.GetMousePosition();
-            if (!TryGetPositionWithinStage(absolutePos, out Vector2Int relativePos))
-                return;
-
-            if (_heldActor == null)
-            {
-                if (!TryFindNewActorForDragging(relativePos))
-                    return;
-            }
-
-            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
-            {
-                _heldActor.RenderBoundingBox();
-
-                var delta = relativePos - _lastMousePosition;
-                _heldActor.Transform.LocalPosition = _heldActor.Transform.LocalPosition + delta;
-
-                _lastMousePosition = relativePos;
-                return;
-            }
-
-            _heldActor = null;
-        }
-
-        /// <summary>
-        /// Tries to get a new actor for dragging.
-        /// </summary>
-        /// <param name="position">The position to hit test from.</param>
-        /// <returns>True if found, false otherwise.</returns>
-        private bool TryFindNewActorForDragging(Vector2Int position)
-        {
-            if (!Raylib.IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
-                return false;
-
-            var actor = VTubeTiny.ActiveStage.HitTest(position);
-            if (actor == null)
-                return false;
-
-            _heldActor = actor;
-            _lastMousePosition = position;
-            return true;
         }
 
         /// <summary>
