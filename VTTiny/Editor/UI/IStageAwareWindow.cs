a3fbb3d32dddbec5f497c9204859108d5a5b7980
naomiEve <0xlunaric@gmail.com>
2022-12-07 11:12:04 +0100

    feat: add the istageawarewindow abstraction.
    every window that will want to respond to stage changes now can
    implement this interface. the editor will call it every time the
    stage changes.

---
+++ VTTiny/Editor/UI/IStageAwareWindow.cs
@@ -0,0 +1,16 @@
+﻿using VTTiny.Scenery;
+
+namespace VTTiny.Editor.UI
+{
+    /// <summary>
+    /// Defines a window that's aware of the current stage and responsive to the stage changes.
+    /// </summary>
+    internal interface IStageAwareWindow
+    {
+        /// <summary>
+        /// Called when the current active stage changes.
+        /// </summary>
+        /// <param name="stage">The new stage.</param>
+        void OnStageChange(Stage stage);
+    }
+}
