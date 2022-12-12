f80bd30afae0ce9e1ec26e24bb7462f296a2d845
naomiEve <0xlunaric@gmail.com>
2022-12-13 00:19:35 +0100

    feat: add KeyToggleableComponent.
    this allows to toggle rendering of an actor after pressing a button.

---
+++ VTTiny/Components/Data/KeyToggleableConfig.cs
@@ -0,0 +1,9 @@
+﻿using Raylib_cs;
+
+namespace VTTiny.Components.Data
+{
+    internal class KeyToggleableConfig
+    {
+        public KeyboardKey ToggleKey { get; set; } = KeyboardKey.KEY_NULL;
+    }
+}
---
+++ VTTiny/Components/KeyToggleableComponent.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using Raylib_cs;
+using VTTiny.Components.Data;
+using VTTiny.Editor;
+
+namespace VTTiny.Components
+{
+    public class KeyToggleableComponent : Component
+    {
+        /// <summary>
+        /// The key that will toggle the rendering of this actor.
+        /// </summary>
+        public KeyboardKey ToggleKey { get; set; } = KeyboardKey.KEY_NULL;
+
+        public override void Update()
+        {
+            if (Raylib.IsKeyPressed(ToggleKey))
+                Parent.AllowRendering = !Parent.AllowRendering;
+        }
+
+        internal override void InheritParametersFromConfig(JObject parameters)
+        {
+            var config = JsonObjectToConfig<KeyToggleableConfig>(parameters);
+            ToggleKey = config.ToggleKey;
+        }
+
+        protected override object PackageParametersIntoConfig()
+        {
+            return new KeyToggleableConfig
+            {
+                ToggleKey = ToggleKey
+            };
+        }
+
+        internal override void RenderEditorGUI()
+        {
+            ToggleKey = EditorGUI.KeycodeDropdown(ToggleKey);
+        }
+    }
+}
