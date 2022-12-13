7cef54ec506e05243f586449af3d7e6a6ac5247d
naomiEve <0xlunaric@gmail.com>
2022-12-13 13:26:50 +0100

    refactor: make use of top level statements and get rid of Main().
    we have C# 10 now, \o/.

--- VTTiny/Program.cs
+++ VTTiny/Program.cs
@@ -1,11 +1,6 @@
 ﻿using CommandLine;
 using VTTiny;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        var parsed = Parser.Default.ParseArguments<VTubeTinyCommandOptions>(args).Value;
-        new VTubeTiny(parsed).Run();
-    }
-}
+Parser.Default
+      .ParseArguments<VTubeTinyCommandOptions>(args)
+      .WithParsed(parsed => new VTubeTiny(parsed).Run());
