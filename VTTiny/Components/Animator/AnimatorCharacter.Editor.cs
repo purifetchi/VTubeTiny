3d98ea41d55d78829a07255f9280fd55de6ea1e7
naomiEve <0xlunaric@gmail.com>
2023-01-04 14:00:24 +0100

    feat!: add the concept of an animator character.
    this is a single config for an animator, that manages its own state.
    it has a bunch of improvements over the initial animator, such as:
    
    1. not enabling states when the textures don't exist (except for idle)
    2. allowing for both speaking and blinking at the same time
    3. being self contained
    
    in the future we could probably implement this as an asset to share
    between animators.

---
+++ VTTiny/Components/Animator/AnimatorCharacter.Editor.cs
@@ -0,0 +1,31 @@
+﻿using VTTiny.Assets;
+using VTTiny.Assets.Management;
+using VTTiny.Editor;
+
+namespace VTTiny.Components.Animator
+{
+    public partial class AnimatorCharacter
+    {
+        /// <summary>
+        /// Draw the editor gui for this character.
+        /// </summary>
+        /// <param name="assetDatabase">The asset database to resolve the textures from.</param>
+        public void DrawEditorGUI(AssetDatabase assetDatabase)
+        {
+            BlinkEvery = EditorGUI.DragFloat("Blink every", BlinkEvery);
+            BlinkLength = EditorGUI.DragFloat("Blink length", BlinkLength);
+
+            if (EditorGUI.AssetDropdown("Idle", assetDatabase, Idle, out Texture newIdle))
+                Idle = newIdle;
+
+            if (EditorGUI.AssetDropdown("Idle blinking", assetDatabase, IdleBlink, out Texture newBlinking))
+                IdleBlink = newBlinking;
+
+            if (EditorGUI.AssetDropdown("Speaking", assetDatabase, Speaking, out Texture newSpeaking))
+                Speaking = newSpeaking;
+
+            if (EditorGUI.AssetDropdown("Speaking blinking", assetDatabase, SpeakingBlink, out Texture newSpeakingBlink))
+                SpeakingBlink = newSpeakingBlink;
+        }
+    }
+}
---
+++ VTTiny/Components/Animator/AnimatorCharacter.cs
@@ -0,0 +1,116 @@
+﻿using VTTiny.Assets;
+
+namespace VTTiny.Components.Animator
+{
+    /// <summary>
+    /// A single character config for an animator.
+    /// </summary>
+    public partial class AnimatorCharacter
+    {
+        /// <summary>
+        /// The state of the character
+        /// </summary>
+        private enum State
+        {
+            Idle,
+            IdleBlink,
+            Speaking,
+            SpeakingBlink
+        }
+
+        /// <summary>
+        /// The frequency of the blinking
+        /// </summary>
+        public float BlinkEvery { get; set; } = 1.5f;
+
+        /// <summary>
+        /// The length of each blink.
+        /// </summary>
+        public float BlinkLength { get; set; } = 0.1f;
+
+        /// <summary>
+        /// The texture for when the character is idling.
+        /// </summary>
+        public Texture Idle { get; set; }
+
+        /// <summary>
+        /// The texture for when the character is idling and blinking.
+        /// </summary>
+        public Texture IdleBlink { get; set; }
+
+        /// <summary>
+        /// The texture for when the character is speaking.
+        /// </summary>
+        public Texture Speaking { get; set; }
+
+        /// <summary>
+        /// The texture for when the character is speaking and blinking.
+        /// </summary>
+        public Texture SpeakingBlink { get; set; }
+
+        /// <summary>
+        /// Whether the character is speaking right now.
+        /// </summary>
+        public bool IsSpeaking { get; set; } = false;
+
+        /// <summary>
+        /// Whether the character is currently blinking.
+        /// </summary>
+        private State _currentState = State.Idle;
+
+        /// <summary>
+        /// The last blink action time.
+        /// </summary>
+        private double _lastBlinkAction = double.MinValue;
+
+        /// <summary>
+        /// Updates the character.
+        /// </summary>
+        /// <param name="time">The current time.</param>
+        public void Update(double time)
+        {
+            var isBlinking = (_currentState == State.IdleBlink) || 
+                             (_currentState == State.SpeakingBlink);
+
+            var delta = time - _lastBlinkAction;
+            if (isBlinking)
+            {
+                if (delta > BlinkLength)
+                {
+                    isBlinking = false;
+                    _lastBlinkAction = time;
+                }
+            }
+            else
+            {
+                if (delta > BlinkEvery)
+                {
+                    isBlinking = true;
+                    _lastBlinkAction = time;
+                }
+            }
+
+            if (isBlinking)
+                _currentState = IsSpeaking ? State.SpeakingBlink : State.IdleBlink;
+
+            else
+                _currentState = IsSpeaking ? State.Speaking : State.Idle; 
+        }
+
+        /// <summary>
+        /// Gets the current texture for the state.
+        /// </summary>
+        /// <returns>The texture.</returns>
+        public Texture GetCurrentStateTexture()
+        {
+            return _currentState switch
+            {
+                State.Idle => Idle,
+                State.IdleBlink when IdleBlink != null => IdleBlink,
+                State.Speaking when Speaking != null => Speaking,
+                State.SpeakingBlink when SpeakingBlink != null => SpeakingBlink,
+                _ => Idle
+            };
+        }
+    }
+}
