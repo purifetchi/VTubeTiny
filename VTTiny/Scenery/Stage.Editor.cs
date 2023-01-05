48d2b19afeccca5ba22c1aa9f6a00f293e2237df
naomiEve <0xlunaric@gmail.com>
2023-01-05 22:11:20 +0100

    refactor: change how the stage editor looks like a bit.

--- VTTiny/Scenery/Stage.Editor.cs
+++ VTTiny/Scenery/Stage.Editor.cs
@@ -11,21 +11,40 @@ namespace VTTiny.Scenery
         /// </summary>
         internal void RenderEditorGUI()
         {
+            RenderStageSettingsGUI();
+
+            ImGui.NewLine();
+
+            RenderActorsEditorGUI();
+        }
+
+        /// <summary>
+        /// Renders the GUI for the stage specific settings.
+        /// </summary>
+        private void RenderStageSettingsGUI()
+        {
+            ImGui.Text("Stage settings");
+
             Dimensions = EditorGUI.DragVector2("Scene dimensions", Dimensions);
             if (ImGui.IsItemDeactivatedAfterEdit())
                 ResizeStage(Dimensions);
 
             ClearColor = EditorGUI.ColorEdit("Clear color", ClearColor);
-            RenderBoundingBoxes = EditorGUI.Checkbox("Render bounding boxes", RenderBoundingBoxes);
-
             TargetFPS = EditorGUI.DragInt("Target FPS", TargetFPS);
             if (ImGui.IsItemDeactivatedAfterEdit())
                 SetTargetFPS(TargetFPS);
 
+            RenderBoundingBoxes = EditorGUI.Checkbox("Render bounding boxes", RenderBoundingBoxes);
             var newBroadcastVal = EditorGUI.Checkbox("Enable Spout Broadcasting", BroadcastViaSpout);
             if (newBroadcastVal != BroadcastViaSpout)
                 SetSpoutOrDefaultContext<FramebufferRenderingContext>(newBroadcastVal);
+        }
 
+        /// <summary>
+        /// Renders the editor gui for the actor picker.
+        /// </summary>
+        private void RenderActorsEditorGUI()
+        {
             ImGui.Text("Actors");
 
             foreach (var actor in _actors)
