08243c5ce2a0aaf9f87591cf5eee8f7e7aeeea93
naomiEve <0xlunaric@gmail.com>
2023-01-06 21:32:29 +0100

    feat: add an interface for components that check for speech.
    this way multiple components can be informed of someone speaking.

---
+++ VTTiny/Components/ISpeakingAwareComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace VTTiny.Components
+{
+    /// <summary>
+    /// The interface for all components that are aware of when the user is speaking.
+    /// 
+    /// The speaking is broadcasted by the AudioResponsiveMovementComponent.
+    /// </summary>
+    public interface ISpeakingAwareComponent
+    {
+        /// <summary>
+        /// Whether the user is speaking.
+        /// </summary>
+        bool IsSpeaking { get; set; }
+    }
+}
