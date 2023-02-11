ee47dd90393269108df3df7f7127b68b987a7e8c
naomiEve <0xlunaric@gmail.com>
2023-02-11 22:47:35 +0100

    feat: add an enumerator for windows audio devices.
    plus add a wrapper for MMDevices to function as IListenableDevices.

---
+++ VTTiny/Audio/Native/Win32/ListenableMMDevice.cs
@@ -0,0 +1,66 @@
+﻿#if ARCH_WINDOWS
+
+using NAudio.CoreAudioApi;
+
+namespace VTTiny.Audio.Native.Win32
+{
+    /// <summary>
+    /// A listenable CoreAudio MMDevice.
+    /// </summary>
+    internal class ListenableMMDevice : IListenableDevice
+    {
+        /// <inheritdoc/>
+        public string Name { get; set; } = "[Unknown]";
+        
+        /// <inheritdoc/>
+        public float Level => _backingDevice.AudioMeterInformation.MasterPeakValue;
+
+        /// <summary>
+        /// The device that backs this listenable device.
+        /// </summary>
+        private readonly MMDevice _backingDevice;
+
+        /// <summary>
+        /// The Windows Audio Session capture of this device.
+        /// </summary>
+        private WasapiCapture _wasapiCapture;
+
+        /// <summary>
+        /// Construct a new ListenableMMDevice from an MMDevice.
+        /// </summary>
+        /// <param name="backingDevice">The MMDevice.</param>
+        public ListenableMMDevice(MMDevice backingDevice)
+        {
+            _backingDevice = backingDevice;
+
+            Name = _backingDevice.FriendlyName;
+        }
+
+        /// <inheritdoc/>
+        public void Listen()
+        {
+            if (_backingDevice.DataFlow == DataFlow.Render)
+                return;
+
+            if (_wasapiCapture != null)
+                return;
+
+            _wasapiCapture = new WasapiCapture(_backingDevice);
+            _wasapiCapture.StartRecording();
+        }
+
+        /// <inheritdoc/>
+        public void Stop()
+        {
+            if (_backingDevice.DataFlow == DataFlow.Render)
+                return;
+
+            if (_wasapiCapture == null)
+                return;
+
+            _wasapiCapture.StopRecording();
+        }
+    }
+}
+
+#endif
---
+++ VTTiny/Audio/Native/Win32/Win32ListenableDeviceEnumerator.cs
@@ -0,0 +1,74 @@
+﻿#if ARCH_WINDOWS
+
+using System.Collections.Generic;
+using NAudio.CoreAudioApi;
+
+namespace VTTiny.Audio.Native.Win32
+{
+    /// <summary>
+    /// Listenable device enumerator for the Win32 architecture.
+    /// </summary>
+    internal class Win32ListenableDeviceEnumerator : IListenableDeviceEnumerator
+    {
+        /// <summary>
+        /// The enumerator for all private devices.
+        /// </summary>
+        private MMDeviceEnumerator DeviceEnumerator { get; set; }
+
+        /// <summary>
+        /// The default device.
+        /// </summary>
+        private IListenableDevice DefaultDevice { get; set; }
+
+        /// <summary>
+        /// Construct a new enumerator.
+        /// </summary>
+        public Win32ListenableDeviceEnumerator()
+        {
+            DeviceEnumerator = new();
+        }
+
+        /// <summary>
+        /// Tries to get the default device.
+        /// </summary>
+        private void TryFindDefaultDevice()
+        {
+            if (DefaultDevice != null)
+                return;
+
+            var maybeDefaultDevice = DeviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+            if (maybeDefaultDevice == null)
+                return;
+
+            DefaultDevice = new ListenableMMDevice(maybeDefaultDevice);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<IListenableDevice> EnumerateAllListenableDevices()
+        {
+            // Try and find the default device, yield if able to.
+            TryFindDefaultDevice();
+            if (DefaultDevice != null)
+                yield return DefaultDevice;
+
+            foreach (var device in DeviceEnumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active))
+            {
+                // We don't wanna yield the default device twice.
+                if (device == DefaultDevice)
+                    continue;
+
+                yield return new ListenableMMDevice(device);
+            }
+        }
+
+        /// <inheritdoc/>
+        public IListenableDevice GetDefaultDevice()
+        {
+            TryFindDefaultDevice();
+            return DefaultDevice;
+        }
+    }
+
+}
+
+#endif
