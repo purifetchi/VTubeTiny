d1fa8d3f93afd5244557ef8ba0f4caea73a58c8e
naomiEve <0xlunaric@gmail.com>
2023-02-11 22:11:52 +0100

    feat: add the concept of a named object.
    now every object that has a name should derive from this.

---
+++ VTTiny/Base/INamedObject.cs
@@ -0,0 +1,13 @@
+﻿namespace VTTiny.Base
+{
+    /// <summary>
+    /// Interface shared by all objects that have a name.
+    /// </summary>
+    public interface INamedObject
+    {
+        /// <summary>
+        /// The name of this object.
+        /// </summary>
+        string Name { get; set; }
+    }
+}
