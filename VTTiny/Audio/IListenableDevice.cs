77d0b190b83f81024a7073a8906b148e01024461
naomiEve <0xlunaric@gmail.com>
2023-02-11 22:34:59 +0100

    feat: add the concept of a listenable device.
    an IListenableDevice is an abstraction over any device that
    exposes an audio level. this can be anything: recording devices,
    rendering devices, even hooks to in-game audio, w/e.
    
    this decouples us from specifically using coreapi everywhere and only
    relegating it to the win32 layer.

---
+++ VTTiny/Audio/IListenableDevice.cs
@@ -0,0 +1,25 @@
+﻿using VTTiny.Base;
+
+namespace VTTiny.Audio
+{
+    /// <summary>
+    /// The generic listenable device interface. Must be inherited by anything that wants to be listenable to.
+    /// </summary>
+    public interface IListenableDevice : INamedObject
+    {
+        /// <summary>
+        /// Starts listening to this device.
+        /// </summary>
+        void Listen();
+        
+        /// <summary>
+        /// Stops listening to this device.
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// The current audio level of this device.
+        /// </summary>
+        float Level { get; }
+    }
+}
---
+++ VTTiny/Audio/IListenableDeviceEnumerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace VTTiny.Audio
+{
+    /// <summary>
+    /// An interface for everything that wants to return an enumerable collection of IListenableDevices.
+    /// </summary>
+    public interface IListenableDeviceEnumerator
+    {
+        /// <summary>
+        /// List all the listenable devices of this enumerator.
+        /// </summary>
+        /// <returns>An IEnumerable of all listenable devices this enumerator has.</returns>
+        IEnumerable<IListenableDevice> EnumerateAllListenableDevices();
+
+        /// <summary>
+        /// Get the default listenable device.
+        /// </summary>
+        /// <returns>The default listenable device.</returns>
+        IListenableDevice GetDefaultDevice();
+    }
+}
