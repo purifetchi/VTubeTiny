6330d0dac4f92b63939c64b0bbd8553226cc19f2
naomiEve <0xlunaric@gmail.com>
2023-02-16 01:28:49 +0100

    feat: add the plugin class.
    this class will contain the loaded plugin assembly and the basic
    plugin metadata.

---
+++ VTTiny/Plugins/Plugin.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace VTTiny.Plugins
+{
+    /// <summary>
+    /// A loaded plugin for VTubeTiny.
+    /// </summary>
+    internal sealed class Plugin
+    {
+        /// <summary>
+        /// The assembly of this plugin.
+        /// </summary>
+        public Assembly PluginAssembly { get; init; }
+
+        /// <summary>
+        /// The name of this plugin.
+        /// </summary>
+        public string Name { get; init; }
+
+        /// <summary>
+        /// The description of this plugin.
+        /// </summary>
+        public string Description { get; init; }
+
+        /// <summary>
+        /// The author of this plugin.
+        /// </summary>
+        public string Author { get; init; }
+
+        /// <summary>
+        /// The version of this plugin.
+        /// </summary>
+        public string Version { get; init; }
+
+        /// <summary>
+        /// Constructs a new plugin, given its assembly.
+        /// </summary>
+        /// <param name="pluginAssembly">The plugin assembly.</param>
+        private Plugin(Assembly pluginAssembly)
+        {
+            PluginAssembly = pluginAssembly;
+        }
+
+        /// <summary>
+        /// Tries to load a new plugin given its path.
+        /// </summary>
+        /// <param name="path">The path to the plugin.</param>
+        /// <returns>Either the loaded plugin, or null if it failed to load.</returns>
+        public static Plugin TryLoadFromFile(string path)
+        {
+            var dllPath = Directory.GetFiles(path)
+                .FirstOrDefault(file => Path.GetExtension(file) == ".dll");
+
+            if (dllPath == null)
+                return null;
+
+            var assembly = Assembly.LoadFrom(dllPath);
+            return new Plugin(assembly);
+        }
+    }
+}
