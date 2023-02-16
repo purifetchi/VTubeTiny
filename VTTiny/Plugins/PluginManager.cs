7f09d75848b161eeb2f9a5a613941780be0ea54b
naomiEve <0xlunaric@gmail.com>
2023-02-16 01:29:22 +0100

    feat: add the plugin manager.
    this class will deal with finding plugins to load, and finding types
    from within them (as it'll be useful later when we'll actually start
    using the loaded plugin assemblies).

---
+++ VTTiny/Plugins/PluginManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace VTTiny.Plugins
+{
+    /// <summary>
+    /// A manager class tasked with loading and storing all of the plugins.
+    /// </summary>
+    internal static class PluginManager
+    {
+        /// <summary>
+        /// The path of the plugins subdirectory.
+        /// </summary>
+        private const string PLUGINS_FOLDER_PATH = "./Plugins/";
+
+        /// <summary>
+        /// The currently loaded plugins.
+        /// </summary>
+        private static List<Plugin> _plugins;
+
+        /// <summary>
+        /// Initializes the manager and loads all of the plugins.
+        /// </summary>
+        public static void LoadAllPlugins()
+        {
+            _plugins = new();
+
+            foreach (var directory in Directory.GetDirectories(PLUGINS_FOLDER_PATH))
+            {
+                var plugin = Plugin.TryLoadFromFile(directory);
+                
+                if (plugin == null)
+                {
+                    Console.WriteLine($"Failed to load plugin {directory}.");
+                    continue;
+                }
+
+                Console.WriteLine($"Loaded plugin {plugin.Name}!");
+                _plugins.Add(plugin);
+            }
+        }
+
+        /// <summary>
+        /// Gets all the currently loaded in assemblies.
+        /// </summary>
+        /// <returns>An enumerable of all of the loaded assemblies (including the main VTubeTiny assembly).</returns>
+        public static IEnumerable<Assembly> GetAllLoadedAssemblies()
+        {
+            // We always want to return the VTubeTiny assembly first, to have priority when loading.
+            yield return Assembly.GetExecutingAssembly();
+
+            foreach (var plugin in _plugins)
+                yield return plugin.PluginAssembly;
+        }
+
+        /// <summary>
+        /// Finds and returns the type given its name in all the loaded assemblies.
+        /// </summary>
+        /// <param name="typeName">The type name.</param>
+        /// <returns>The type if it was found, or null.</returns>
+        public static Type FindTypeInLoadedAssemblies(string typeName)
+        {
+            foreach (var assembly in GetAllLoadedAssemblies())
+            {
+                var type = assembly.GetType(typeName);
+                
+                if (type != null)
+                    return type;
+            }
+            
+            return null;
+        }
+    }
+}
