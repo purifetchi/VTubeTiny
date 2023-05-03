ef703635389bcfa531cc011623e2149cf1d5ce11
naomiEve <0xlunaric@gmail.com>
2023-05-03 21:57:40 +0200

    feat: add the concept of a guidrawable.
    this should be inherited by anything that wants to draw a gui in editor.

---
+++ VTTiny/Editor/IEditorGUIDrawable.cs
@@ -0,0 +1,12 @@
+﻿namespace VTTiny.Editor;
+
+/// <summary>
+/// Implemented by all objects which want their GUI to be drawn.
+/// </summary>
+public interface IEditorGUIDrawable
+{
+    /// <summary>
+    /// Draw the editor GUI for this object.
+    /// </summary>
+    void RenderEditorGUI();
+}
